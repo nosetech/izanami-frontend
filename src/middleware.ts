5034bf9c4a0e44c7d373b67d144a291ce01de1c1
Hiroyuki Nose <80944036+nosetech@users.noreply.github.com>
2025-08-25 09:17:38 +0900

    認証状態をチェックするミドルウェアの実装 (#3)

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export default function auth(request: NextRequest) {
+  const user = request.cookies.get('user')
+
+  if (!user) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+// TODO: 対象となるパスを整備する
+export const config = {
+  matcher: ['/graphql-client/:path*'],
+}
