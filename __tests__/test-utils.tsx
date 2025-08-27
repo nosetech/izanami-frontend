e54a4951606ae2883de4dc886ec2463e79380fbf
nosetech <system.nosetech@gmail.com>
2025-08-27 15:38:26 +0900

    コード警告の修正

--- __tests__/test-utils.tsx
+++ __tests__/test-utils.tsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { ThemeProvider } from '@mui/material/styles'
 import { defaultTheme } from '@/theme/default'
+import { ThemeProvider } from '@mui/material/styles'
+import { render } from '@testing-library/react'
+import React from 'react'
+import renderer from 'react-test-renderer'
 
 // カスタムレンダー関数（ThemeProviderでラップ）
 export const renderWithTheme = (ui: React.ReactElement) => {
-  return render(
-    <ThemeProvider theme={defaultTheme}>
-      {ui}
-    </ThemeProvider>
-  )
+  return render(<ThemeProvider theme={defaultTheme}>{ui}</ThemeProvider>)
 }
 
 // react-test-rendererも修正
 export const createWithTheme = (ui: React.ReactElement) => {
-  const renderer = require('react-test-renderer')
   return renderer.create(
-    <ThemeProvider theme={defaultTheme}>
-      {ui}
-    </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>{ui}</ThemeProvider>,
   )
 }
